0fa8555539f069fc3ce5fff77680ad26b0de50e2
ukit-tokyo <t.yuki@ukit.tokyo>
2024-10-29 22:06:00 +0900

    make project

---
+++ TCATutorials/TCATutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsApp.swift
+//  TCATutorials
+//
+//  Created by Taichi Yuki on 2024/10/29.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
