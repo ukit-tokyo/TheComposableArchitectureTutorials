29771bae9c5cc0d649e3ad18c323b73ec648474a
ukit-tokyo <t.yuki@ukit.tokyo>
2024-10-29 23:28:30 +0900

    Adding side effects

--- TCATutorials/CounterFeature.swift
+++ TCATutorials/CounterFeature.swift
@@ -13,11 +13,22 @@ struct CounterFeature {
   @ObservableState
   struct State {
     var count: Int = 0
+    var fact: String?
+    var isLoading: Bool = false
+    var isTimerRunning: Bool = false
   }
 
   enum Action {
     case decrementButtonTapped
+    case factButtonTapped
+    case factResponse(String)
     case incrementButtonTapped
+    case timerTick
+    case toggleTimerButtonTapped
+  }
+
+  enum CancelID {
+    case timer
   }
 
   var body: some ReducerOf<Self> {
@@ -25,11 +36,49 @@ struct CounterFeature {
       switch action {
       case .decrementButtonTapped:
         state.count -= 1
+        state.fact = nil
+        return .none
+
+      case .factButtonTapped:
+        state.fact = nil
+        state.isLoading = true
+
+        return .run { [count = state.count] send in
+          let (data, _) = try await URLSession.shared
+            .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+          let fact = String(decoding: data, as: UTF8.self)
+          await send(.factResponse(fact))
+        }
+
+      case .factResponse(let fact):
+        state.fact = fact
+        state.isLoading = false
         return .none
 
       case .incrementButtonTapped:
         state.count += 1
+        state.fact = nil
         return .none
+
+      case .timerTick:
+        state.count += 1
+        state.fact = nil
+        return .none
+
+      case .toggleTimerButtonTapped:
+        state.isTimerRunning.toggle()
+        if state.isTimerRunning {
+          return .run { send in
+            while true {
+              try await Task.sleep(for: .seconds(1))
+              await send(.timerTick)
+            }
+          }
+          .cancellable(id: CancelID.timer)
+        }
+        else {
+          return .cancel(id: CancelID.timer)
+        }
       }
     }
   }
@@ -65,6 +114,31 @@ struct CounterView: View {
         .padding()
         .background(Color.black.opacity(0.1))
         .cornerRadius(10)
+      }
+
+      Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+        store.send(.toggleTimerButtonTapped)
+      }
+      .font(.largeTitle)
+      .padding()
+      .background(Color.black.opacity(0.1))
+      .cornerRadius(10)
+
+      Button("Fact") {
+        store.send(.factButtonTapped)
+      }
+      .font(.largeTitle)
+      .padding()
+      .background(Color.black.opacity(0.1))
+      .cornerRadius(10)
+
+      if store.isLoading {
+        ProgressView()
+      } else if let fact = store.fact {
+        Text(fact)
+          .font(.largeTitle)
+          .multilineTextAlignment(.center)
+          .padding()
       }
     }
   }
