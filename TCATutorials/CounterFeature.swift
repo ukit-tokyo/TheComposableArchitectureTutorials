4f3a8d58a87442f112f2e9bc283031e572883edd
ukit-tokyo <t.yuki@ukit.tokyo>
2024-10-30 01:10:47 +0900

    Testing your feature

--- TCATutorials/CounterFeature.swift
+++ TCATutorials/CounterFeature.swift
@@ -11,7 +11,7 @@ import ComposableArchitecture
 struct CounterFeature {
 
   @ObservableState
-  struct State {
+  struct State: Equatable {
     var count: Int = 0
     var fact: String?
     var isLoading: Bool = false
@@ -31,6 +31,9 @@ struct CounterFeature {
     case timer
   }
 
+  @Dependency(\.continuousClock) var clock
+  @Dependency(\.numberFact) var numberFact
+
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
@@ -44,9 +47,7 @@ struct CounterFeature {
         state.isLoading = true
 
         return .run { [count = state.count] send in
-          let (data, _) = try await URLSession.shared
-            .data(from: URL(string: "http://numbersapi.com/\(count)")!)
-          let fact = String(decoding: data, as: UTF8.self)
+          let fact = try await self.numberFact.fetch(count)
           await send(.factResponse(fact))
         }
 
@@ -69,8 +70,7 @@ struct CounterFeature {
         state.isTimerRunning.toggle()
         if state.isTimerRunning {
           return .run { send in
-            while true {
-              try await Task.sleep(for: .seconds(1))
+            for await _ in self.clock.timer(interval: .seconds(1)) {
               await send(.timerTick)
             }
           }
